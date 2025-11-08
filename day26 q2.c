494d5c6885d2299ddedbdf23e084d390998367e8
devangmourya53-dev <devangmourya53@gmail.com>
2025-11-08 17:43:01 +0530

    Add files via upload

---
+++ day26 q2.c
@@ -0,0 +1,36 @@
+/* Write a program to print the following pattern:
+
+*
+
+*
+*
+*
+
+*
+*
+*
+*
+*
+
+*
+*
+*
+
+*
+
+*/
+#include <stdio.h>
+int main() 
+{
+    int blocks[] = {1, 3, 5, 3, 1};
+    int i,j;
+    for(i=0;i<5;i++)
+     {
+        for(j=0;j<blocks[i];j++)
+         {
+            printf("*\n");
+        }
+        printf("\n");
+    }
+    return 0;
+}
